6c1e8440365332b5783a52cffd432ee0e941e9de
assisjrcontador-art <assisjr.contador@gmail.com>
2026-09-25 00:35:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aa-financas-v34-syncfix-2';
+const CACHE='aa-financas-v35-final-1';
 const CORE=['./','./index.html','./manifest.json','./aa-cover.png','./icon-192.png','./icon-512.png','https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.49.8/dist/umd/supabase.min.js'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
